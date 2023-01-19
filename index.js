b00287d7bff07253ec94dfc2b0742572950ab354
clxxiii <clxxiii@users.noreply.github.com>
2023-01-18 22:52:11 -0500

    normalize for methods other than sharex

--- index.js
+++ index.js
@@ -7,45 +7,43 @@ require("dotenv").config();
 const app = express();
 
 const {
-	STRING_LENGTH: fileNameLength,
-	HOSTNAME: hostname,
-	SECRET: secret,
-	PORT: port 
+  STRING_LENGTH: fileNameLength,
+  HOSTNAME: hostname,
+  SECRET: secret,
+  PORT: port,
 } = process.env;
 
 app.use(fileUpload());
-app.use(express.static('files'));
+app.use(express.static("files"));
 
 app.post("/upload", (req, res) => {
-	const recieved = req.body.secret;
+  const recieved = req.body.secret;
 
-	if (secret != recieved) {
-		res.status(403).end();
-		return;
-	}
+  if (secret != recieved) {
+    res.status(403).end();
+    return;
+  }
 
+  const file = req.files.file;
+  const type = mime.extension(file.mimetype);
+  let string = random(fileNameLength);
 
-	const file = req.files.sharex;
-	const type = mime.extension(file.mimetype);
-	let string = random(fileNameLength);
+  while (fs.existsSync(`files/${string}.${type}`)) {
+    string = random(fileNameLength);
+  }
 
-	while (fs.existsSync(`files/${string}.${type}`)) {
-		string = random(fileNameLength)
-	}
-
-	fs.writeFileSync(`files/${string}.${type}`, file.data);
-	console.log(`Uploaded ${hostname}${string}.${type}`);
-	res.status(200).send(`${hostname}${string}.${type}`);
-})
+  fs.writeFileSync(`files/${string}.${type}`, file.data);
+  console.log(`Uploaded ${hostname}/${string}.${type}`);
+  res.status(200).send(`${hostname}/${string}.${type}`);
+});
 
 app.listen(port);
 
-
 const random = (n) => {
-	const validChars = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
-	let string = "";
-	for (let i = 0; i < n; i++) {
-		string += validChars.charAt(Math.floor(Math.random() * validChars.length));
-	}
-	return string;
-}
+  const validChars = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+  let string = "";
+  for (let i = 0; i < n; i++) {
+    string += validChars.charAt(Math.floor(Math.random() * validChars.length));
+  }
+  return string;
+};
